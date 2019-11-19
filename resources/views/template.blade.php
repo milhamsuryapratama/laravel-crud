63ed63ec0489ac8a2d4d1ca37ec2d3ace474b30d
milhamsuryapratama <ilhamsuryapratama.github@gmail.com>
2019-11-19 15:18:50 +0700

    menambah template pada view

---
+++ resources/views/template.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Index</title>
+</head>
+<body>
+
+    <ul>
+        <li><a href="/pegawai">Pegawai</a></li>
+        <li><a href="/jurusan">Jurusan</a></li>
+    </ul>
+
+    <br/>
+
+    @yield('konten')
+
+</body>
+</html>
