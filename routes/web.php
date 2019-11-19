02e9a402710a7a9aea359ebf16203b7298cbd9e2
milhamsuryapratama <ilhamsuryapratama.github@gmail.com>
2019-11-19 15:21:53 +0700

    menambah jurusan route

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,10 @@ Route::post('/pegawai/store', 'PegawaiController@store');
 Route::get('/pegawai/edit/{id}', 'PegawaiController@edit');
 Route::post('/pegawai/update', 'PegawaiController@update');
 Route::get('/pegawai/hapus/{id}', 'PegawaiController@hapus');
+
+Route::get('/jurusan', 'JurusanController@index');
+Route::get('/jurusan/tambah', 'JurusanController@tambah');
+Route::post('/jurusan/simpan', 'JurusanController@simpan');
+Route::get('/jurusan/edit/{id}', 'JurusanController@edit');
+Route::post('/jurusan/update', 'JurusanController@update');
+Route::get('/jurusan/hapus/{id}', 'JurusanController@hapus');
