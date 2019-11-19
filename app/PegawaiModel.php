c57ca74d8fe545fe0a948ef31dbba23d76700217
milhamsuryapratama <ilhamsuryapratama.github@gmail.com>
2019-11-19 15:35:54 +0700

    menambah jurusan_id pada fillable pegawai model

--- app/PegawaiModel.php
+++ app/PegawaiModel.php
@@ -8,7 +8,7 @@ class PegawaiModel extends Model
 {
     protected $table = 'pegawai';
     protected  $primaryKey = 'pegawai_id';
-    protected $fillable = ['pegawai_nama','pegawai_jk','pegawai_jabatan','pegawai_umur','pegawai_alamat'];
+    protected $fillable = ['pegawai_nama','pegawai_jk','pegawai_jabatan','pegawai_umur','pegawai_alamat','jurusan_id'];
     public $incrementing = false;
     public $timestamps = true;
 }
